14e8d2b25225ea57d7581efdece770858894e489
Douglas Teixeira <100793013+dougsteixeira@users.noreply.github.com>
2022-03-12 09:17:52 -0300

    Add files via upload

---
+++ Genesis.js
@@ -0,0 +1,109 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 = verde
+//1 = vermelho
+//2 = amarelo
+//3 = azul
+
+ const blue = document.querySelector('.blue');
+ const red = document.querySelector('.red');
+ const green = document.querySelector('.green');
+ const yellow = document.querySelector('.yellow');
+
+ // Criando ordem aleatória
+
+ function shuffleOrder() {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for (let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+
+}
+
+ // Acende a próxima cor
+ let lightColor = (element, Number) => {
+     Number = Number * 500;
+     setTimeout(() => {
+         element.classlist.add('selected');
+         
+     }, Number - 250);
+     setTimeout(() => {
+         element.classlist.remove('selected');
+     });
+
+}
+
+// Checa se os clicks estão corretos     
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert("Pontuação: ${score}\n Você acertou! Iniciando novo nível!")
+        nextlevel();
+    }
+}
+
+//Função para click do usuário
+
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classlist.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classlist.remove('selected');
+        checkOrder();    
+    },250);
+
+}
+
+//Função que retorna a cor
+let createColorElement = (color) => {
+    if(color == 0){
+        return green;
+    }else if(color == 1) {
+        return red;
+    }else if(color == 2){
+        return yellow;
+    }else if (color == 3) {
+        return blue;
+    }    
+}
+
+let nextlevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+
+let gameOver = () => {
+    alert("Pontuação: ${score}!\n Você perdeu o jogo!\n Clique em OK para iniciar um novo jogo!");
+    order = [];
+    clickedOrder = [];
+
+    playgame();
+}
+
+let playgame = () => {
+    alert("Bem vindo ao Gênesis! Iniciando um novo jogo!")
+    score = 0;
+    nextlevel();
+}
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+ 
+
+
+playgame();
